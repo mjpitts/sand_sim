3892d864c4dfd4d1d617ce0d6cb1920feb18574f
mjpitts <mjpitts@ucdavis.edu>
2023-10-13 11:53:42 -0700

    Cleaned commented Grid.h

--- sand_sim/Grid.h
+++ sand_sim/Grid.h
@@ -11,9 +11,17 @@
 class Grid
 {
 private:
+	/*
+		Number of pixels in heightand width per tile,
+		- Tile dim = TILESIZE x TILESIZE.
+	*/ 
 	const float TILESIZE = 25;
+
+	// Number of tiles in the window, Width then Height.
 	float mapSizeW = 0;
 	float mapSizeH = 0;
+
+	// All the tiles to be rendered.
 	std::vector<std::vector<sf::RectangleShape>> gridMap;
 
 
@@ -27,9 +35,11 @@ public:
 	const float getTileSize() const;
 	std::vector<std::vector<sf::RectangleShape>> getGridMap();
 
-	// Functions
+	// Init functions
 	void initVars(sf::VideoMode& vm);
 	void initGrid();
+
+	// Fuunctions
 	void renderGrid(sf::RenderTarget* target);
 };
 #endif
